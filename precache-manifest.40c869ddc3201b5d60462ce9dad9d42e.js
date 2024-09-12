eb41be86110a1b3e36f756e7630704fb4862e93d
Emmie05 <emmvicadforster@gmail.com>
2024-09-12 16:37:40 +0000

    Updates

---
+++ precache-manifest.40c869ddc3201b5d60462ce9dad9d42e.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ef89121dd3579ae19bb28bb67431c761",
+    "url": "/Portfolio-Project.git/index.html"
+  },
+  {
+    "revision": "ec906474d0244d77b494",
+    "url": "/Portfolio-Project.git/static/css/main.d14f0fec.chunk.css"
+  },
+  {
+    "revision": "9eab7c22f6e4c8c8d3de",
+    "url": "/Portfolio-Project.git/static/js/2.2503002b.chunk.js"
+  },
+  {
+    "revision": "b95080709ac12d0967db165d90e6c04a",
+    "url": "/Portfolio-Project.git/static/js/2.2503002b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ec906474d0244d77b494",
+    "url": "/Portfolio-Project.git/static/js/main.8cb19037.chunk.js"
+  },
+  {
+    "revision": "bf59187e5eda4a1a04cd",
+    "url": "/Portfolio-Project.git/static/js/runtime-main.d52bfb3c.js"
+  },
+  {
+    "revision": "40f5dabc436668bd31cbe895fac37852",
+    "url": "/Portfolio-Project.git/static/media/color_code-day.40f5dabc.jpg"
+  },
+  {
+    "revision": "fb2ddaf46f3e181940383239085a24e9",
+    "url": "/Portfolio-Project.git/static/media/color_code-night.fb2ddaf4.jpg"
+  },
+  {
+    "revision": "24d7e35f4f371260702f0cde83e01652",
+    "url": "/Portfolio-Project.git/static/media/day.24d7e35f.png"
+  },
+  {
+    "revision": "635174257f99a19d1e1c5fda2156814d",
+    "url": "/Portfolio-Project.git/static/media/gpa-day.63517425.jpg"
+  },
+  {
+    "revision": "52fd494b7e0ccce72726ea64bd4eafb6",
+    "url": "/Portfolio-Project.git/static/media/gpa-night.52fd494b.jpg"
+  },
+  {
+    "revision": "dd9a2800bd8072423ba40574450e6071",
+    "url": "/Portfolio-Project.git/static/media/home-light.dd9a2800.jpg"
+  },
+  {
+    "revision": "9baae8021b184b8b442cf088c91f4e59",
+    "url": "/Portfolio-Project.git/static/media/home-night.9baae802.jpg"
+  },
+  {
+    "revision": "95dc3388db42670e0d29bbd43c9ad692",
+    "url": "/Portfolio-Project.git/static/media/logo-black.95dc3388.png"
+  },
+  {
+    "revision": "922d0f6518f473fa6a13caabbe664b4e",
+    "url": "/Portfolio-Project.git/static/media/logo-white.922d0f65.png"
+  }
+]);
