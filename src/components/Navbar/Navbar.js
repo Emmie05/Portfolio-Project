d3a7bba1e1c13b6bbf9280d903fc89fc2c8cdc90
Emmie05 <emmvicadforster@gmail.com>
2024-09-17 14:38:24 +0000

    Refactor Navbar component to add dark mode support and update documentation

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -6,6 +6,15 @@ import logo_dark from '../../assets/logo-white.png';
 import toggle_light from '../../assets/night.png';
 import toggle_dark from '../../assets/day.png';
 
+/**
+ * Navbar component that displays a navigation bar with a logo and a theme toggle icon.
+ *
+ * @param {Object} props - The component props.
+ * @param {string} props.theme - The current theme, either 'light' or 'dark'.
+ * @param {Function} props.setTheme - Function to toggle the theme between 'light' and 'dark'.
+ *
+ * @returns {JSX.Element} The rendered Navbar component.
+ */
 const Navbar = ({ theme, setTheme }) => {
   const toggleMode = () => {
     setTheme((prevTheme) => (prevTheme === 'light' ? 'dark' : 'light'));
