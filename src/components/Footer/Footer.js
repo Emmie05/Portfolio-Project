59347ec2a5dd025f9314380a44d2bef186af77c0
Emmie05 <emmvicadforster@gmail.com>
2024-09-17 14:35:02 +0000

    Refactor Footer component and update social media links and copyright information

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -2,17 +2,20 @@ import React from 'react';
 import { FaTwitter, FaLinkedin, FaGithub, FaInstagram } from 'react-icons/fa';
 import './Footer.css'
 
-
+// Footer component definition
 const Footer = () => {
   return (
     <div className='FooterContainer'>
+      {/* Display the name */}
       <p>Emmanuel Forster</p>
+      
+      {/* Social media links */}
       <div className='SocialLinks'>
         <a href="https://www.linkedin.com/in/emmanuel-forster-3ab072296" target="_blank" rel="noopener noreferrer">
-        <FaLinkedin />
+          <FaLinkedin />
         </a>
         <a href="https://github.com/Emmie05" target="_blank" rel="noopener noreferrer">
-        <FaGithub />
+          <FaGithub />
         </a>
         <a href="https://twitter.com/EmmForster" target="_blank" rel="noopener noreferrer">
           <FaTwitter />
@@ -21,9 +24,12 @@ const Footer = () => {
           <FaInstagram />
         </a>
       </div>
+      
+      {/* Copyright information */}
       <p>&copy; 2024 Alx Africa. All rights reserved.</p>
     </div>
   );
 };
 
+// Export the Footer component as the default export
 export default Footer;
